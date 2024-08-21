6bddc75ecb656f754730b96f71989f29c48e7dd5
Björn Schrammel <github@schrammel.dev>
2024-08-21 14:41:13 +0200

    Addding typehints and docstrings

--- src/plugins/parquet_viewer/overview.py
+++ src/plugins/parquet_viewer/overview.py
@@ -1,8 +1,32 @@
+from typing import TYPE_CHECKING
+
 import wx
+
+if TYPE_CHECKING:
+    from src.plugins.parquet_viewer import ParquetViewer
 
 
 class Overview(wx.Panel):
-    def __init__(self, parent):
+    """
+    The Overview Panel for the Parquet Viewer
+
+    This panel contains an overview of the Parquet file.
+
+    The overview contains:
+    - Total Rows
+    - Total Columns
+    - Column Names
+
+    The overview is read-only and cannot be edited.
+    """
+    def __init__(self, parent: wx.Panel) -> None:
+        """
+        Initialize the Overview Panel
+
+        :param parent: The parent panel
+        :type parent: wx.Panel
+        :return: None
+        """
         super().__init__(parent)
         self.__sizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.__sizer)
@@ -10,7 +34,13 @@ class Overview(wx.Panel):
         self.__base_info = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2 | wx.TE_NO_VSCROLL)
         self.__sizer.Add(self.__base_info, 1, wx.EXPAND)
 
-    def update(self, plugin):
+    def update(self, plugin: "ParquetViewer") -> None:
+        """
+        Update the Overview Panel with the information from the Parquet Viewer
+
+        :param plugin: The Parquet Viewer plugin
+        :return: None
+        """
         self.__base_info.Clear()
         self.__base_info.AppendText(f"Total Rows: {plugin.get_total_rows()}\n")
         self.__base_info.AppendText(f"Total Columns: {len(plugin.get_relation().columns)}\n")
