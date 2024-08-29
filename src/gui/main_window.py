54c7ab3924b5a9195968654ce2d22b446eb6f482
Björn Schrammel <github@schrammel.dev>
2024-08-29 21:38:18 +0200

    Lots of random fixes

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,4 +1,5 @@
 import threading
+from enum import Enum
 from pathlib import Path
 from typing import Tuple, TYPE_CHECKING
 
@@ -11,15 +12,21 @@ if TYPE_CHECKING:
     from src.engine import Environment
 
 
+class SettingMenuIName(Enum):
+    """The MenuItem enum."""
+    RELOAD_PLUGINS = 0
+    ABOUT = 1
+    QUIT = 2
+
 
 class GuiApplication(wx.App):
-    def __init__(self, environment: "Environment"):
+    def __init__(self, environment: "Environment") -> None:
         self.environment = environment
         self.logger = self.environment.logger.getChild("gui")
         self.plugin_lock = threading.Lock()
         super().__init__(False)
 
-    def OnInit(self):
+    def OnInit(self) -> bool:
         # Create the main window
         self.root_frame = wx.Frame(None, title=self.environment.get("application", {}).get("name", "Application"))
 
@@ -54,20 +61,21 @@ class GuiApplication(wx.App):
         self.root_frame.Show()
         return True
 
-    def OnExit(self):
+    def OnExit(self) -> bool:
         self.logger.debug("Destroying GuiApplication")
         return True
 
-    def OnAbout(self, event):
-        wx.MessageBox("This is a wxPython application\n\nIcon made by Freepik from www.flaticon.com", "About", wx.OK | wx.ICON_INFORMATION)
+    def OnAbout(self, event: "wx.Event") -> bool:
+        msg = (f"Second Brain\n\n Version: {self.environment.get('application', {}).get('version', '0.0.1')}\n")
+        wx.MessageBox(msg, "About", wx.OK | wx.ICON_INFORMATION)
         return True
 
-    def OnQuit(self, event):
+    def OnQuit(self, event: "wx.Event") -> bool:
         self.OnExit()
         self.root_frame.Close()
         return True
 
-    def OnPluginStart(self, event):
+    def OnPluginStart(self, event: "wx.Event") -> None:
         plugin_name = self.menu_bar.GetLabel(event.GetId())
         plugin = next((plugin for plugin in self.environment.plugins if plugin.name == plugin_name), None)
         if plugin:
@@ -86,55 +94,78 @@ class GuiApplication(wx.App):
             else:
                 self.logger.warning("Another plugin is already running")
 
-    def OnReloadPlugins(self, event):
+    def OnReloadPlugins(self, event: "wx.Event") -> bool:
         """Reload all plugins from the plugins folder"""
         for plugin in self.environment.plugins:
             plugin.stop()
-            del plugin
 
         self.environment.plugins = self.environment.reload_plugins()
         self.logger.info("Plugins reloaded")
         return True
 
-    def OnForceClosePlugin(self, event):
+    def OnForceClosePlugin(self, event: "wx.Event") -> bool:
         """Force close the running plugin"""
         if hasattr(self, "active_plugin"):
             self.active_plugin.stop()
             self.logger.info("Active plugin closed")
-            self.plugin_thread.join()
-            del self.active_plugin
+            self.plugin_thread.join(5)
+            self.active_plugin = None
         else:
             self.logger.warning("No plugin is running")
         return True
 
-    def __setup_menu(self):
+    def __setup_menu(self) -> bool:
+        """
+        Setup the menu bar. This method creates the menu bar and adds the plugin and settings menus.
+
+        Returns:
+            bool: True if the menu bar was successfully setup, False otherwise
+        """
         self.menu_bar = wx.MenuBar()
         self.root_frame.SetMenuBar(self.menu_bar)
+        self.menu_bar.Append(self.__setup_plugin_menu(), "&Plugins")
+        self.menu_bar.Append(self.__setup_settings_menu(), "&Settings")
 
+    def __setup_plugin_menu(self) -> wx.Menu:
+        """
+        Setup the plugin menu. This method creates the plugin menu and adds the plugins to the menu.
+
+        Returns:
+            wx.Menu: The plugin menu
+        """
         plugins = wx.Menu()
-        self.menu_bar.Append(plugins, "&Plugins")
-
         for plugin in self.environment.plugins:
-            plugin_menu = plugins.Append(wx.ID_ANY, plugin.name)
-            self.root_frame.Bind(wx.EVT_MENU, self.OnPluginStart, plugin_menu)
+            plugins.Append(
+                wx.ID_ANY,
+                plugin.name
+            )
+            self.root_frame.Bind(wx.EVT_MENU, self.OnPluginStart, plugins.FindItemById(wx.ID_ANY))
+        return plugins
 
+    def __setup_settings_menu(self) -> wx.Menu:
+        """
+        Setup the settings menu. This method creates the settings menu and adds the settings to the menu.
+
+        Returns:
+            wx.Menu: The settings menu
+        """
         settings = wx.Menu()
-        self.menu_bar.Append(settings, "&Settings")
+        for setting in SettingMenuIName:
+            settings.Append(
+                setting.value,
+                setting.name.replace("_", " ").title()
+            )
+            func_ = getattr(self, f"On{setting.name.title().replace('_', '')}")
+            self.root_frame.Bind(wx.EVT_MENU, func_, settings.FindItemById(setting.value))
+        return settings
 
-        reload_plugins_item = settings.Append(wx.ID_ANY, "&Reload Plugins", "Reload all plugins")
-        self.root_frame.Bind(wx.EVT_MENU, self.OnReloadPlugins, reload_plugins_item)
+    def __setup_plugin_frame(self) -> bool:
+        """
+        Setup the plugin frame. This method creates the plugin frame and sets the background color.
 
-        about_item = settings.Append(wx.ID_ABOUT, "&About", "Information about this application")
-        self.root_frame.Bind(wx.EVT_MENU, self.OnAbout, about_item)
-
-        force_close_plugin_item = settings.Append(wx.ID_ANY, "&Close Active Plugin", "Force close the running plugin")
-        self.root_frame.Bind(wx.EVT_MENU, self.OnForceClosePlugin, force_close_plugin_item)
-
-        quit_item = settings.Append(wx.ID_EXIT, "&Quit", "Quit this application")
-        self.root_frame.Bind(wx.EVT_MENU, self.OnQuit, quit_item)
-        return True
-
-    def __setup_plugin_frame(self):
+        Returns:
+            bool: True if the plugin frame was successfully setup, False otherwise
+        """
         self.plugin_frame = wx.Panel(self.root_frame)
         self.plugin_frame.SetBackgroundColour(wx.Colour(WINDOW_BACKGROUND))
 
@@ -145,12 +176,21 @@ class GuiApplication(wx.App):
         self.environment['plugin_frame'] = self.plugin_frame
         return True
 
-    def __setup_status_bar(self):
+    def __setup_status_bar(self) -> bool:
+        """
+        Setup the status bar. This method creates the status bar and sets the default text.
+
+        Returns:
+            bool: True if the status bar was successfully setup, False otherwise
+        """
         self.environment['status_bar'] = self.root_frame.CreateStatusBar()
 
         # Create a space to display running sub-processes
         self.environment['status_bar'].SetFieldsCount(2)
-        self.environment['status_bar'].SetStatusWidths([-1, 200])
+        self.environment['status_bar'].SetStatusWidths([
+            -1,
+            self.environment["application"].get("status_bar", {}).get("sub_process", {}).get("width", 200)
+        ])
 
         # Set the default text for the status bar
         self.environment['status_bar'].SetStatusText("Main thread ready", 0)
@@ -161,13 +201,25 @@ class GuiApplication(wx.App):
 
     @staticmethod
     def __center_on_all_monitors(frame: wx.Frame) -> Tuple:
-        # Get the number of available displays
+        """
+        Center the frame on all monitors. This method calculates the center of all monitors and centers the frame on the
+        monitor that contains the center of the frame.
+
+        Args:
+            frame: The frame to center
+
+        Returns:
+            Tuple: The position of the frame
+        """
+        # Get the number of displays
         num_displays = wx.Display.GetCount()
 
         top = 0
         bottom = 0
         leftmost = 0
         rightmost = 0
+
+        # Get the dimensions of all displays
         for i in range(num_displays):
             display = wx.Display(i)
             x, y, width, height = display.GetGeometry()
