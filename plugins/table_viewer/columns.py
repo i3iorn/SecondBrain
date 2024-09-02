12d1be7f88db1a7a21bebb3a973153e14bd8cd8a
Björn Schrammel <github@schrammel.dev>
2024-09-02 16:11:01 +0200

    Added more column information.

--- plugins/table_viewer/columns.py
+++ plugins/table_viewer/columns.py
@@ -1,8 +1,12 @@
+import threading
 from typing import TYPE_CHECKING
-from .helpers import status_message
-from .components.panel import BasePanel
 
+import duckdb
 import wx
+import wx.grid
+
+from .components.panel import BasePanel
+from .helpers import status_message
 
 if TYPE_CHECKING:
     from plugins.table_viewer import TableViewer
@@ -31,28 +35,81 @@ class ColumnOverviewPanel(BasePanel):
             parent (wx.Panel): The parent panel for the Overview Panel.
         """
         super().__init__(tv.panel)
+        self.plugin = tv
         self.status_bar = tv.status_bar
         self.__sizer = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.__sizer)
 
-        self.__base_info = wx.TextCtrl(self, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.TE_RICH2)
-        self.__sizer.Add(self.__base_info, 1, wx.EXPAND)
+        self.setup_ui()
 
-    @status_message(f"Updating overview")
-    def update(self, columns: list) -> None:
+        self.Show()
+
+    def setup_ui(self):
         """
-        Update the Overview Panel with the information from the Table Viewer.
-
-        This method clears the text control and populates it with the total number of rows, total number of columns,
-        and the column names from the Table Viewer plugin.
-
-        Args:
-            columns (list): The list of column names from the Table Viewer plugin.
+        Set up the UI for the Column Overview Panel.
+        Returns:
+            bool: True if the UI was successfully set up, False otherwise.
         """
-        for i, column in enumerate(columns):
-            self.__base_info.AppendText(f"{i + 1}. {column}\n")
+        self.info_grid = wx.grid.Grid(self)
+        self.info_grid.CreateGrid(0, 3)
 
-        self.__base_info.AppendText("\n")
+        self.info_grid.SetColLabelValue(0, "Column Name")
+        self.info_grid.SetColLabelValue(1, "Coverage")
+        self.info_grid.SetColLabelValue(2, "Unique Values")
 
+        self.__sizer.Add(self.info_grid, 1, wx.EXPAND)
+
+        return True
+
+    def update(self):
+        update_thread = threading.Thread(target=self.update_thread)
+        update_thread.start()
+
+    @status_message("Updating column overview", 1)
+    def update_thread(self) -> bool:
+        """
+        Update the overview information in the text control.
+        """
+        self.info_grid.ClearGrid()
+
+        if self.info_grid.GetNumberRows() > 0:
+            self.info_grid.DeleteRows(0, self.info_grid.GetNumberRows())
+
+        if self.plugin.grid.df is None:
+            return False
+        df = self.plugin.grid.df
+
+        total_rows = len(df)
+        if total_rows >= 10000000:
+            wx.MessageBox("The file is too large to display all unique values. 10 million rows will be used as a sample.", "Warning", wx.OK | wx.ICON_WARNING)
+            rows_to_check = 10000000
+        else:
+            rows_to_check = total_rows
+
+        for i, column in enumerate(df.columns):
+            while True:
+                try:
+                    column_values = df[column].limit(rows_to_check).fetchall()
+                    break
+                except duckdb.InvalidInputException:
+                    continue
+
+            self.info_grid.AppendRows(1)
+            self.info_grid.SetCellValue(i, 0, column)
+            self.info_grid.SetCellValue(i, 1, f"{len(column_values) / rows_to_check:.2%}")
+            self.info_grid.SetCellValue(i, 2, f"{len(set(column_values))}")
+
+        self.info_grid.AutoSize()
         self.Layout()
-        self.Refresh()
+
+        return True
+
+    def on_label_click(self, event):
+        label = event.GetRow()
+        print(label)
+        column = self.info_grid.GetCellValue(label, 0)
+        coverage = self.info_grid.GetCellValue(label, 1)
+        unique = self.info_grid.GetCellValue(label, 2)
+
+        self.status_bar.SetStatusText(f"Column: {column}, Coverage: {coverage}, Unique Values: {unique}", 1)
+        event.Skip()
