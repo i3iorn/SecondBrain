339d4cae058e614199381f74a826515148589dd4
Björn Schrammel <github@schrammel.dev>
2024-09-02 16:04:46 +0200

    Removed custom sizer placement to increase customization

--- plugins/table_viewer/components/panel.py
+++ plugins/table_viewer/components/panel.py
@@ -7,16 +7,5 @@ from .mixins import SetFontMixin
 class BasePanel(SetFontMixin, wx.Panel):
     def __init__(self, parent: wx.Panel) -> None:
         super().__init__(parent)
-        self.parent_sizer = parent.GetSizer().GetItem(0).GetSizer()
-        self.parent_sizer.Add(self, 1, wx.EXPAND)
-
         self.SetForegroundColour(COMPONENT_FOREGROUND)
-
         self.set_font()
-
-        self.__outer_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.SetSizer(self.__outer_sizer)
-
-    @property
-    def outer_sizer(self):
-        return self.__outer_sizer
