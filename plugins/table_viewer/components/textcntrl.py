83260ea098d034331f4d1dfdd47d4dc7dbacba65
bjosch <github@bjosch.tk>
2024-09-05 08:54:46 +0200

    Added hover effects

--- plugins/table_viewer/components/textcntrl.py
+++ plugins/table_viewer/components/textcntrl.py
@@ -1,5 +1,7 @@
 import wx
 
+from config.colors import *
+
 
 class TVTextCntrl(wx.TextCtrl):
     def __init__(self, parent, *args, **kwargs):
@@ -8,3 +10,17 @@ class TVTextCntrl(wx.TextCtrl):
 
         self.parent_sizer = parent.GetSizer()
         self.parent_sizer.Add(self, 1, wx.EXPAND)
+
+        self.SetBackgroundColour(TEXTBOX_BACKGROUND)
+        self.SetForegroundColour(TEXTBOX_FOREGROUND)
+
+        self.Bind(wx.EVT_ENTER_WINDOW, self.on_hover)
+        self.Bind(wx.EVT_LEAVE_WINDOW, self.on_leave)
+
+    def on_hover(self, event):
+        self.SetBackgroundColour(TEXTBOX_BACKGROUND_HOVER)
+        event.Skip()
+
+    def on_leave(self, event):
+        self.SetBackgroundColour(TEXTBOX_BACKGROUND)
+        event.Skip()
